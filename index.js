b4535876b21fcc4b5c45b3e984ca6af2ec0d34dc
Erik <erik@apption.com>
2021-04-21 20:05:19 -0400

    copied junk code from random site ♥

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "!"
+const prefix = "!";
+const roleName = '2 Month Supporter';
+
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
+
 client.on('message', message => {
-    if(message.content.startsWith(`${prefix}tpwhois`)){
+    //if(message.content.startsWith(`${prefix}tpwhois`)){
         var Member = message.mentions.members.first()
         if(!Member){
             var Member = message.member
@@ -16,16 +19,24 @@ client.on('message', message => {
         if(status == "dnd"){
             var status = "Do not Disturb"
         }
-        let whoisEmbed = new Discord.MessageEmbed()
-            .setTitle(`Who is ${Member.username}?`)
+        var joinedSince = new Date() - Member.joinedAt
+        let differentDays = Math.round(joinedSince / (1000 * 3600 * 24));
+        if (differentDays >= 60 && !Member.roles.cache.some(role => role.name === roleName))
+        {
+            const role = message.guild.roles.cache.find(role => role.name === roleName);
+            Member.roles.add(role);
+            let whoisEmbed = new Discord.MessageEmbed()
+            .setTitle(`Hey ${Member.displayName}!`)
             .setColor("PURPLE")
-            .setDescription(`ID: ${Member.id}`)
-            .addField("Joined at", Member.joinedAt)
-            .addField("Status", status)
-            .addField("Created at", Member.createdAt)
-            .setFooter(d)
-        message.channel.send(whoisEmbed)
-    }
+            .setDescription(`you are getting upgraded`)
+            .addField("Joined since", differentDays)
+//            .addField("Joined at", Member.joinedAt)
+//            .addField("Status", status)
+            .setFooter("Turtlebot")
+            message.channel.send(whoisEmbed)
+        }
+
+    //}
 });
 
 client.login('ODMxNzEyNjI2NjI2MTM0MDM3.YHZOvg.GeF2mztB7iDGvqpP14YTGEEGSmI');
