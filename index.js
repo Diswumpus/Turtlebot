ba073b7104e807fd8a1c337aa91633fa5d7fc19d
TurtlePaw <turtlesimsyt@gmail.com>
2021-04-23 17:19:15 -0400

    Bot reacts to msgs

--- index.js
+++ index.js
@@ -73,7 +73,45 @@ client.on('message', message => {
     
 
     }
+    if (message.content === (`${config.prefix}ping`)) {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('Pong.');
+    }
+    if (message.content === (`OMG`)) {
+        message.react('833491346018271253');
+    }
+    if (message.content === (`Yay!`)) {
+        message.react('831584687498461274');
+    }
+    if (message.content === (`Yay`)) {
+        message.react('831584687498461274');
+    }
+    if (message.content === (`Good morning everyone`)) {
+        message.react('835250126087389194');
+    }
+    if (message.content === (`good morning everyone`)) {
+        message.react('835250126087389194');
+    }
+    if (message.content === (`Goodmorning everyone`)) {
+        message.react('835250126087389194');
+    }
+    if (message.content === (`goodmorning everyone`)) {
+        message.react('835250126087389194');
+    }
+    if (message.content === (`Goodnight everyone`)) {
+        message.react('835250124703400026');
+    }
+    if (message.content === (`goodnight everyone`)) {
+        message.react('835250124703400026');
+    }
+    if (message.content === (`Good night everyone`)) {
+        message.react('835250124703400026');
+    }
+    if (message.content === (`good night everyone`)) {
+        message.react('835250124703400026');
+    }
 });
+
 client.on('guildMemberAdd', member => { //This is creating an event saying when a member joins the server...
     
     const channel = member.guild.channels.cache.find(ch => ch.name.includes("welcome")); //** This is telling the script which server to send teh message in**\\
