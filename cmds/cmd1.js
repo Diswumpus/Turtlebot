016079eda1f73a43b53159614d7882b1dad4d546
TurtlePaw <turtlesimsyt@gmail.com>
2021-04-25 12:44:53 -0400

    Fixed some stuff

--- cmds/cmd1.js
+++ cmds/cmd1.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 module.exports = {
     name: 'tp',
     description: 'A blanc cmd',
--- cmds/test.js
+++ cmds/test.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 module.exports = {
     name: 'test',
     description: 'Test',
