78db260292413d2867c846993333d10f11d764e8
TurtlePaw <turtlesimsyt@gmail.com>
2021-04-27 14:15:21 -0400

    Added 8 ball cmd

---
+++ cmds/8ball.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+  name: "8ball",
+  description: "There is a big chance I insult you!",
+  category: "fun",
+  execute(message, Member, args) {
+    let question = message.content.slice(bot.prefix.length + 6);
+    if (!question)
+      return message.channel.send(`You did not specify your question!`);
+    else {
+      let responses = [
+        "Yes",
+        "No",
+        "Definetly",
+        "Absoloutely",
+        "Not in a million years",
+      ];
+      let response =
+        responses[Math.floor(Math.random() * responses.length - 1)];
+      let Embed = new MessageEmbed()
+        .setTitle(`8Ball!`)
+        .setDescription(`Your question: ${question}\nMy reply: ${response}`)
+        .setColor(`RANDOM`);
+      message.channel.send(Embed);
+    }
+  },
+};
