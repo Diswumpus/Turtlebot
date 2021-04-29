19ae930672dba212ce6a7498aff8a725b41386c7
TurtlePaw <turtlesimsyt@gmail.com>
2021-04-29 19:34:23 -0400

    Added title

--- cmds/announce.js
+++ cmds/announce.js
@@ -3,22 +3,25 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
   name: 'announce',
   description: "Get the bot to say what ever you want in a specific channel.",
-  usage: "<channel id> <msg>",
+  usage: "<channel id> <msg> <title>",
   execute: async (message, Member, args) => {
     let rChannel = message.guild.channels.cache.get(args[0]);
     if (!rChannel)
       return message.channel.send(
         `You did not specify your channel to send the announcement too!`
       );
-    console.log(rChannel);
-    let MSG = message.content
-      .split(`announce ${rChannel.id} `)
-      .join("");
-    if (!MSG)
+    //console.log(rChannel);
+    let msg = args[1]
+    let title = args[2]
+    // let MSG = message.content
+    //   .split(`announce ${rChannel.id} `)
+    //   .join("");
+     
+    if (!msg)
       return message.channel.send(`You did not specify your message to send!`);
     const _ = new MessageEmbed()
-      .setTitle(`New announcement!`)
-      .setDescription(`${MSG}`)
+      .setTitle(`${title}`)
+      .setDescription(`${msg}`)
       .setColor("RANDOM");
     rChannel.send(_);
     message.delete();
--- index.js
+++ index.js
@@ -72,6 +72,7 @@ client.on('message', message => {
             try {
                 command.execute(message, Member, args);
             } catch (error) {
+                console.error("yikes!!");
                 console.error(error);
                 message.reply("sorry I'm not feeling well - I think it is the pizza from last night");
             }
@@ -101,13 +102,7 @@ client.on('guildMemberAdd', member => { //This is creating an event saying when 
     const serverName = member.guild.name
     if (!channel) return;
     var blob1 = client.emojis.cache.get('835250126087389194')
-    const welcomeEmbed = new Discord.MessageEmbed()
-	.setColor('AQUA')
-	.setTitle(`Welcome to ${serverName} ${blob1}`)
-	.setDescription(`please read the <#824406474804953109>, hope you have a pleasant stay ${member}! Say ${config.prefix}verify to begin! ${member}`)
-	.setThumbnail(`<${user.displayAvatarURL({ dynamic: true })}>`)
-	.setFooter('Turtlebot', 'turtlebot.url');
-    channel.send(welcomeEmbed);
+    channel.send(`Welcome to ${serverName} ${blob1}, please read the <#824406474804953109>, hope you have a pleasant stay ${member}! Say ${config.prefix}verify to begin! ${member}`);
 }); // That up ^here^ tells the bot what channel to send the message in!
 client.login(config.token);
 //client.user.setActivity('-help');
