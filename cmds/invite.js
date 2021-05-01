912bb02aa3a3d618f7a234543f5e7e1fe9dac111
TurtlePaw <turtlesimsyt@gmail.com>
2021-04-30 22:50:06 -0400

    Problem with the Emoji

--- cmds/invite.js
+++ cmds/invite.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'invite',
-    description: 'Invite me for dinner',
+    description: 'Generates an invite for your server',
     async execute(message, Member, args) {
         let invite = await message.channel.createInvite({
             maxAge: 0, // 0 = infinite expiration
             maxUses: 0 // 0 = infinite uses
           }).catch(console.error);
-        message.channel.send(`Here's your invite! ${invite}`);
+        message.channel.send(`Here's an invite for ${message.guild.name}! ${invite}`);
     },
 };
