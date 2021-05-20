6260c9448a47dab625f7fd22e927b38995f24310
TurtlePaw <turtlesimsyt@gmail.com>
2021-05-19 20:21:11 -0400

    Added stuff

--- slash/avatar.js
+++ slash/avatar.js
@@ -3,7 +3,11 @@ const Discord = require('discord.js');
 module.exports = {
     name: 'avatar',
     async execute(client, interaction) {
-        const member = interaction.options[0].member;        
-        await interaction.reply(member.user.displayAvatarURL());
+        const member = interaction.options[0].member;
+        const avatarembed = new Discord.MessageEmbed()
+        .setTitle(`Avatar for ${member.user.tag}`)
+        .addField(`Link as:`, `[png](${member.user.displayAvatarURL({ dynamic: true, format: 'png', size: 1024 })}) | [jpg](${member.user.displayAvatarURL({ dynamic: true, format: 'jpg', size: 1024 })}) | [webp](${member.user.displayAvatarURL({ dynamic: true, format: 'webp', size: 1024 })})`)
+        .setImage(member.user.displayAvatarURL({ dynamic: true, size: 1024 }))
+        await interaction.reply(avatarembed);
     }
 }
