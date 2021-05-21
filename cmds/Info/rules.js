d2e5d018f1e7b1baa80015038d3a429d7c1b1584
TurtlePaw <turtlesimsyt@gmail.com>
2021-05-20 22:00:15 -0400

    Added/fixed rules

--- cmds/Info/rules.js
+++ cmds/Info/rules.js
@@ -3,27 +3,24 @@ const Discord = require('discord.js');
 module.exports = {
     name: 'rules',
     category: 'Info',
-    description: 'This command is for help for the submit commmand',
+    description: 'TURTLEPAWS SERVER ONLY',
     execute(message, Member, args) {
         const ver = message.client.emojis.cache.find(em => em.name === "verify");
         const turtlebot = message.client.emojis.cache.find(em => em.name === "Turtlebot");
         const blobpar = message.client.emojis.cache.find(em => em.name === "Party_blob");
         const blobwave = message.client.emojis.cache.find(em => em.name === "blobwave");
         const serverName = Member.guild.name
-        // let rEmbed = new Discord.MessageEmbed()
-        //     .setTitle()
-        //     .setColor("AQUA")
-        //     //        .setDescription(`${ver}`)
-        //     .addField()
-        //     .setFooter(`Turtlebot`, turtlebot.url)
-        message.channel.send(`Welcome to ${serverName} ${blobwave}\n` + `The rules are:
-        - No NSFW or NSFL
-        - No overruling the admins or owner
-        - Please respect the admins and the owners
-        - No arguing
-        - No spam if you want to spam do it in <#836292487936606258>
-         Have fun! ${blobpar}
-          
-            Use ,verify to begin! ${ver}`);
+        const rules = new Discord.MessageEmbed()
+        .setTitle(`Welcome to ${serverName} ${blobwave}`)
+        .setColor(`RED`)
+        .setDescription(`The rules are:`)
+        .addField(`Rule 1`, `No NSFW or NSFL`)
+        .addField(`Rule 2`, `No overruling the admins or owner`)
+        .addField(`Rule 3`, `Please respect the admins and the owners`)
+        .addField(`Rule 4`, `No arguing`)
+        .addField(`Rule 5`, `No spam if you want to spam do it in <#836292487936606258>`)
+        .addField(`Rule 6:`, `Follow Discords ToS`)
+        .setFooter(`Have fun!`, blobpar.url)
+        message.channel.send(rules);
     },
 }; 
