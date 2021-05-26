357ab95195bcc654b24446fff1d51b77ce213fab
TurtlePaw <turtlesimsyt@gmail.com>
2021-05-26 17:47:37 -0400

    Fixed Lockdown

--- cmds/Moderation/lockdown.js
+++ cmds/Moderation/lockdown.js
@@ -2,40 +2,43 @@ const Discord = require('discord.js');
 const ms = require("ms");
 
 module.exports = {
-  name: 'lockdown',
-  category: 'Moderation',
-  description: 'Locks the chat',
-  execute(message, Member, args) {
-    if (!message.guild) return;
-    if (message.member.permissions.has('MANAGE_MESSAGES')) {
-      let time = args[0]
-      if (!args[0]){
-        message.guild.roles.cache.forEach(role => {
-            message.channel.updateOverwrite(role, {
-               SEND_MESSAGES: false
+   name: 'lockdown',
+   category: 'Moderation',
+   description: 'Locks the chat',
+   async execute(message, Member, args) {
+      if (!message.guild) return;
+      if (message.member.permissions.has('MANAGE_MESSAGES')) {
+         let time = args[0]
+         if (!args[0]) {
+            message.guild.roles.cache.forEach(role => {
+               message.channel.updateOverwrite(role, {
+                  SEND_MESSAGES: false
+               })
             })
-         })
-         const lockembed = new Discord.MessageEmbed()
-         .setTitle(`:white_check_mark: Locked down ${message.channel.name}`)
-      message.channel.send(lockembed)
-        }
-        if (args[0]){
-          message.guild.roles.cache.forEach(role => {
-            message.channel.updateOverwrite(role, {
-               SEND_MESSAGES: false
+            const lockembed = new Discord.MessageEmbed()
+               .setTitle(`:white_check_mark: Locked down ${message.channel.name}`)
+            const m = await message.channel.send(lockembed)
+         }
+         if (args[0]) {
+            message.guild.roles.cache.forEach(role => {
+               message.channel.updateOverwrite(role, {
+                  SEND_MESSAGES: false
+               })
             })
-         })
-         const lockembed = new Discord.MessageEmbed()
-         .setTitle(`:white_check_mark: Locked down ${message.channel.name} for ${time}`)
-        message.channel.send(lockembed)
-        setTimeout(() => {
-                message.guild.roles.cache.forEach(role => {
-            message.channel.updateOverwrite(role, {
-               SEND_MESSAGES: true
-            })
-         })
-        },ms(args[0]))
-        }
-    }
-  },
+            const lockembed = new Discord.MessageEmbed()
+               .setTitle(`:white_check_mark: Locked down ${message.channel.name} for ${time}`)
+            const m = await message.channel.send(lockembed)
+            setTimeout(() => {
+               const unlock = new Discord.MessageEmbed()
+                  .setTitle(`:white_check_mark: Unlocked ${message.channel.name}`)
+               m.edit(unlock)
+               message.guild.roles.cache.forEach(role => {
+                  message.channel.updateOverwrite(role, {
+                     SEND_MESSAGES: true
+                  })
+               })
+            }, ms(args[0]))
+         }
+      }
+   },
 };
