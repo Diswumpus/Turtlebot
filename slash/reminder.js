3b7357a07075975898f3d654eb409a9dc2c685ef
TurtlePaw <turtlesimsyt@gmail.com>
2021-05-27 22:24:07 -0400

    -

---
+++ slash/reminder.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const ms = require('ms')
+
+module.exports = {
+    name: 'reminder',
+    description: '-',
+    async execute(client, interaction) {
+        const time = interaction.options[0].value;
+        const reminder = interaction.options[1].value;
+        const timer = client.emojis.cache.get('846868929065517066');
+        const embedd = new Discord.MessageEmbed()
+        .setTitle(`Reminder > ${reminder}`)
+        .setAuthor(`Author > ${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true, size: 1024 }))
+        .setFooter(`${time}`, timer.url)
+        .setColor("AQUA")
+        .setTimestamp()  
+        await interaction.reply(embedd);
+        setTimeout(() => {
+            const embeddd = new Discord.MessageEmbed()
+            .setTitle(`Reminder > ${reminder}`)
+            .setAuthor(`Author > ${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true, size: 1024 }))
+            .setDescription(`Time's Up! ${timer}`)
+            .setColor("AQUA")
+            .setTimestamp()
+            interaction.channel.send(embeddd)
+            interaction.user.send(embeddd)
+        }, ms(time))
+    }
+}
