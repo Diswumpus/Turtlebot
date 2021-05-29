ad78b1f4f2c646b88e1c5d3b0ab54e9ce750d328
TurtlePaw <turtlesimsyt@gmail.com>
2021-05-28 20:10:25 -0400

    -

--- cmds/Config/rsetup.js
+++ cmds/Config/rsetup.js
@@ -6,24 +6,23 @@ module.exports = {
     category: 'Config',
     description: `Creates the ${r2msName} role`,
     execute(message, Member, args) {
-
-        var guild = Member.guild;
-        const blob8 = message.client.emojis.cache.find(em => em.name === "meow_cheer");
-        var r2ms = guild.roles.cache.find(r => r.name === r2msName);
-        if (!r2ms)
-        {
-            // Create a new role with data and a reason
-            guild.roles.create({
-                data: {
-                    name: '2 Month Supporter',
-                    color: 'RANDOM',
-                },
-                reason: 'Required for the give role to user after 2 month',
-            })
-                .then(console.log)
-                .catch(console.error);
-            message.reply(`Successfully created! ${blob8}`);
+        if (message.member.permissions.has('MANAGE_MESSAGES')) {
+            var guild = Member.guild;
+            const blob8 = message.client.emojis.cache.find(em => em.name === "meow_cheer");
+            var r2ms = guild.roles.cache.find(r => r.name === r2msName);
+            if (!r2ms) {
+                // Create a new role with data and a reason
+                guild.roles.create({
+                    data: {
+                        name: '2 Month Supporter',
+                        color: 'RANDOM',
+                    },
+                    reason: 'Required for the give role to user after 2 month',
+                })
+                    .then(console.log)
+                    .catch(console.error);
+                message.reply(`Successfully created! ${blob8}`);
+            }
         }
-        
     }
 }
--- slash/clear.js
+++ slash/clear.js
@@ -3,10 +3,12 @@ const Discord = require('discord.js');
 module.exports = {
     name: 'clear',
     async execute(client, interaction) {
-        let deletenum = interaction.options[0].value;
-        const embeedd = new Discord.MessageEmbed()
-            .setTitle(`:wastebasket: Deleted ${deletenum} messages!`)
-        interaction.channel.bulkDelete(deletenum)
-        await interaction.reply(embeedd);
+        if (interaction.member.permissions.has('MANAGE_MESSAGES')) {
+            let deletenum = interaction.options[0].value;
+            const embeedd = new Discord.MessageEmbed()
+                .setTitle(`:wastebasket: Deleted ${deletenum} messages!`)
+            interaction.channel.bulkDelete(deletenum)
+            await interaction.reply(embeedd);
+        }
     }
 }
