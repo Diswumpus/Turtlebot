3fe2b92a0f90d809cf92ee7728774688bd7ac08a
TurtlePaw <turtlesimsyt@gmail.com>
2021-05-28 20:12:52 -0400

    -

--- slash/stats.js
+++ slash/stats.js
@@ -11,6 +11,7 @@ module.exports = {
         .setThumbnail(turtlebot.url)
         .addField(`Uptime:`, `${client.uptime/1000 /60 /60 /24}`)
         .addField(`Guilds:`, `${client.guilds.cache.size}`)
+        .addField(`Invite:`, `[Here](https://discord.com/oauth2/authorize?client_id=831712626626134037&permissions=4228381815&scope=bot%20applications.commands)`)
         .addField(`Code:`, `[Here](https://github.com/TurtlePaw/Turtlebot)`)
         .addField(`Developer`, `[Turtlepaw#5377](https://discord.com/channels/@me/847987367557333002)`)
         .addField(`Support Server`, `[Click here](https://discord.com/invite/5Wutrs8s4s)`)
