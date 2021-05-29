00487ad98d8a005e6a4f1708649d3241d559b7c8
TurtlePaw <turtlesimsyt@gmail.com>
2021-05-28 22:34:34 -0400

    -

--- slash/stats.js
+++ slash/stats.js
@@ -9,7 +9,7 @@ module.exports = {
         const embeedd = new Discord.MessageEmbed()
         .setColor('AQUA')
         .setThumbnail(turtlebot.url)
-        .addField(`Uptime:`, `${client.uptime/1000 /60 /60 /24}`)
+        .addField(`Uptime:`, `${client.uptime/1000 /60 /60 /24} days`)
         .addField(`Guilds:`, `${client.guilds.cache.size}`)
         .addField(`Invite:`, `[Here](https://discord.com/oauth2/authorize?client_id=831712626626134037&permissions=4228381815&scope=bot%20applications.commands)`)
         .addField(`Code:`, `[Here](https://github.com/TurtlePaw/Turtlebot)`)
