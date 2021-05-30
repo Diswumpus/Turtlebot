99accc7b9d2ee691eef27e28ffc30d6edb068bcb
TurtlePaw <turtlesimsyt@gmail.com>
2021-05-30 18:21:13 -0400

    -

--- slash/guilds.js
+++ slash/guilds.js
@@ -2,11 +2,9 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'guilds',
+    description: 'Gives a hint',
     async execute(client, interaction) {
         const guilds = client.guilds.cache.map(g=>g.name).join('\n• ')
-        const guildsembed = new Discord.MessageEmbed()
-        .setTitle(`These are all the guilds!`)
-        .addField(`• ${guilds}`)
-        await interaction.reply(guildsembed);
+        await interaction.reply(`• ${guilds}`); 
     }
 }
