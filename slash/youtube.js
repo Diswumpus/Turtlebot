8c5345ca3c372a3c39ec04a51a532043c6f37105
TurtlePaw <turtlesimsyt@gmail.com>
2021-06-04 16:08:12 -0400

    ?

--- slash/youtube.js
+++ slash/youtube.js
@@ -7,7 +7,10 @@ module.exports = {
   async execute(client, interaction) {
     if (interaction.member.permissions.has('MANAGE_MESSAGES')) {
       let ytuser = interaction.options.length > 1 ? interaction.options[1].user : interaction.user;
+      console.log('Comment', interaction.options[0])
+      console.log('Comment2', interaction.options[0].value)
       let comment = interaction.options[0].value;
+      console.log(comment)
       await interaction.defer();
       let link = (`https://some-random-api.ml/canvas/youtube-comment?username=${ytuser.username}&comment=${comment}&avatar=${ytuser.displayAvatarURL({ dynamic: true, format: 'png', size: 1024 })}&dark=true%E2%80%8B`)
         const attachment = new Discord.MessageAttachment(link, 'youtube.png');
