4b5be2f96975882fa10085bb5e7cafcadedafb8a
TurtlePaw <turtlesimsyt@gmail.com>
2021-06-04 21:42:53 -0400

    ?

--- cmds/Fun/[.js
+++ cmds/Fun/[.js
@@ -1,22 +1,22 @@
-var randomWords = require('random-words');
-const Discord = require('discord.js');
-const { ShuffleGuess } = require('weky');
-module.exports = {
-    name: 'oo',
-    description: 'A embed template',
-    category: 'Fun',
-    async execute(message, Member, args) {
-    const word = randomWords();
-    const game = new ShuffleGuess({
-                message: message,
-                word: word, //or a simple word
-                winMessage: "GG you won!", //message sent when user's message matches with the word
-                colorReshuffleButton: 'green', //color of the reshuffle button (regen)
-                messageReshuffleButton: 'reshuffle', //text of the reshuffle button (regen)
-                colorCancelButton: 'red', //color of the cancel button (exit, quit, stop)
-                messageCancelButton: 'cancel', //text of the cancel button
-                client: message.client
-    });
-    game.start();
-    },
-};
+// var randomWords = require('random-words');
+// const Discord = require('discord.js');
+// const { ShuffleGuess } = require('weky');
+// module.exports = {
+//     name: 'oo',
+//     description: 'A embed template',
+//     category: 'Fun',
+//     async execute(message, Member, args) {
+//     const word = randomWords();
+//     const game = new ShuffleGuess({
+//                 message: message,
+//                 word: word, //or a simple word
+//                 winMessage: "GG you won!", //message sent when user's message matches with the word
+//                 colorReshuffleButton: 'green', //color of the reshuffle button (regen)
+//                 messageReshuffleButton: 'reshuffle', //text of the reshuffle button (regen)
+//                 colorCancelButton: 'red', //color of the cancel button (exit, quit, stop)
+//                 messageCancelButton: 'cancel', //text of the cancel button
+//                 client: message.client
+//     });
+//     game.start();
+//     },
+// };
