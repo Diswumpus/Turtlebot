311797f5a2296c95f5756297edba46824b532544
TurtlePaw <turtlesimsyt@gmail.com>
2021-06-18 20:56:09 -0400

    Add rules?

--- cmds/Info/rules.js
+++ cmds/Info/rules.js
@@ -9,7 +9,7 @@ module.exports = {
             message.author.send(`You don't have permissions!`)
             return message.delete();
         }
-        const channel = message.client.channels.cache.get('824406474804953109');
+        const channel = message.channel;//message.client.channels.cache.get('824406474804953109');
         const webhooks = await channel.fetchWebhooks();
         const webhook = webhooks.first();
         const icon = 'https://cdn.discordapp.com/icons/824365717573992480/b251af56c4d6c5d4f6ec8be0cc040bb1.png?size=4096'
@@ -23,17 +23,15 @@ module.exports = {
         .setTitle(`Welcome to ${serverName}`)
         .setColor(`RED`)
         .setDescription(`The rules are:`)
-        .addField(`Rule 1`, `No NSFW or NSFL`)
-        .addField(`Rule 2`, `No overruling the admins or owner`)
-        .addField(`Rule 3`, `Please respect the admins and the owners`)
-        .addField(`Rule 4`, `No arguing`)
-        .addField(`Rule 5`, `No spam if you want to spam do it in <#836292487936606258>`)
-        .addField(`Rule 6:`, `Follow Discords ToS`)
+        .addField(`Rule 1`, `No NSFW or anything like that!`, true)
+        .addField(`Rule 2`, `No spam if you want to spam do it in <#836292487936606258>`, true)
+        .addField(`Rule 3:`, `Follow Discords ToS`, true)
+        .addField('Rule 4: English Only', `\`\`\`We can only effectively moderate in English. Please keep all discussion in English, or it will be deleted! In the future, we may add support for a global chatroom.\`\`\``, true)
         .setFooter(`Have fun!`, blobpar.url)
-        // await webhook.send({
-        //     username: 'A Turtle',
-        //     avatarURL: 'https://cdn.discordapp.com/emojis/635256471675994132.gif?v=1',
-        //     embeds: [rules],
-        // });
+        await webhook.send({
+            username: 'A Turtle',
+            avatarURL: 'https://cdn.discordapp.com/emojis/635256471675994132.gif?v=1',
+            embeds: [rules],
+        });
     },
 }; 
