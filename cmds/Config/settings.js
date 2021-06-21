f429f75cc419795baa0c9cad5c1f1ae7f41a99e1
TurtlePaw <turtlesimsyt@gmail.com>
2021-06-21 18:28:53 -0400

    .

--- cmds/Config/settings.js
+++ cmds/Config/settings.js
@@ -13,14 +13,17 @@ module.exports = {
             const tembed = new Discord.MessageEmbed()
                 .setTitle(`What would you like to edit?`)
                 .setDescription(`Use ${config.prefix}enable \`setting\` to enable a setting`)
+                .addField(`\`${config.prefix}enable-link\``, `Delete discord invite links?`)
                 .addField(`\`${config.prefix}enable-welcomemsg\` Welcome Messages?`, "`True or false`")
                 .addField(`\`${config.prefix}prefix\` Prefix?`, `What prefix?`)
-                .addField(`\`${config.prefix}enable-ban\` Ban Messages?`, `Should i send messages when members get banned? ||(With reason)||`)
+                .addField(`\`${config.prefix}enable-snipe\` Auto Snipe?`, `When a message is deleted i will post it in the mentioned channel`)
+                .setColor(message.client.confiig.color)
+            const sentmsg = await message.channel.send({ embeds: [tembed] });
+            /*
+                            .addField(`\`${config.prefix}enable-ban\` Ban Messages?`, `Should i send messages when members get banned? ||(With reason)||`)
                 .addField(`\`${config.prefix}enable-welcomedm\` Welcome DMs?`, "`True or false`")
                 .addField(`\`${config.prefix}welcomedmmsg\` Welcome Messages?`, `What should i send to the user?`)
-                .addField(`\`${config.prefix}enable-snipe\` Auto Snipe?`, `When a message is deleted i will post it in the \`bot-log\``)
-                .setColor(message.client.confiig.color)
-            const sentmsg = await message.channel.send(tembed)
+                */
         }
     }
 }
