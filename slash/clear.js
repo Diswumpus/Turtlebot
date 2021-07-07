ddb23f91158447819b7dfabd623fc7c40ce9db20
TurtlePaw <turtlesimsyt@gmail.com>
2021-07-06 22:01:50 -0400

    Fix clear

--- slash/clear.js
+++ slash/clear.js
@@ -6,10 +6,13 @@ module.exports = {
         if (interaction.member.permissions.has('MANAGE_MESSAGES')) {
             let deletenum = interaction.options?.find(c => c?.name === 'amount')?.value;
             const embeedd = new Discord.MessageEmbed()
-                .setTitle(`:wastebasket: Deleted ${deletenum} messages!`)
+                .setTitle(`${require('../emojis.json').check} Deleted ${deletenum} messages!`)
                 .setColor(client.confiig.color)
             interaction.channel.bulkDelete(deletenum)
             await interaction.reply({ embeds: [embeedd] });
+            setTimeout(async () => {
+                await interaction.deleteReply();
+            }, 2000);
         }
     }
 }
