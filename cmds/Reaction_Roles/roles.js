8d7208121fa0331ec4a19acc914b185df4e683e2
TurtlePaw <turtlesimsyt@gmail.com>
2021-07-08 21:22:35 -0400

    Added ,roles

---
+++ cmds/Reaction_Roles/roles.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'roles',
+    category: 'Reaction Roles',
+    description: 'See how many users have a role!',
+    async execute(message, Member, args) {
+        //Check perms
+        if (!message.member.permissions.has('MANAGE_MESSAGES')) return
+        //Reply.
+        let role;
+        if(message.mentions.roles.first()){
+            role = message.mentions.roles.first()
+        } else {
+            role = message.guild.roles.cache.get(args[0]);
+        }
+        const embed = new Discord.MessageEmbed()
+        .setTitle(`Users with role: ${role.name}`)
+        .setDescription(`${role.members.map(m => m.user)}`)
+        .setColor(message.client.confiig.color)
+        message.channel.send({ embeds: [embed] });
+    },
+};
