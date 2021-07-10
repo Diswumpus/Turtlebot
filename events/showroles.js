bf0bc578ef47cf8b0a965ad7a8800bc2a5418acb
TurtlePaw <turtlesimsyt@gmail.com>
2021-07-09 20:20:51 -0400

    Fix sr

--- events/showroles.js
+++ events/showroles.js
@@ -6,9 +6,24 @@ module.exports = {
 	async execute(interaction, client) {
 		if (!interaction.isButton()) return;
             if(interaction.customId === 'showrolesbt'){
+                const roles = new Array();
+                const rfind = await brs.find({
+                    guild: interaction.guild.id
+                });
+                rfind.forEach(r => {
+                    if(interaction.member.roles.cache.has(r.role1)){
+                        roles.push(r.role1)
+                    }
+                    if(interaction.member.roles.cache.has(r.role2)){
+                        roles.push(r.role2)
+                    }
+                    if(interaction.member.roles.cache.has(r.role3)){
+                        roles.push(r.role3)
+                    }
+                });
                 const embed = new Discord.MessageEmbed()
                 .setTitle('Your roles')
-                .setDescription(`${interaction.member.roles.cache.map(a => a) || 'None'}`)
+                .setDescription(`${roles.map(a => `<@&${a}>`) || 'None'}`)
                 .setColor(client.confiig.color)
             await interaction.reply({ embeds: [embed], content: `Your roles`, ephemeral: true });
             } else {
