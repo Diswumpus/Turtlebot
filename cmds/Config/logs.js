2946bb76e38e1893a77c3b5ecbf6e98693542efd
TurtlePaw <turtlesimsyt@gmail.com>
2021-07-10 23:29:36 -0400

    Add remove logs

--- cmds/Config/logs.js
+++ cmds/Config/logs.js
@@ -20,12 +20,25 @@ module.exports = {
     category: 'Config',
     description: 'Changes the bots prefix',
     async execute(message, Member, args) {
+        const logs = await lModel.find({ guild: message.guild.id })
+        const logarr = new Array()
+        const logco = new Discord.Collection()
+        let inum = 0
+        logs.forEach(e => {
+            inum++
+            logco.set(inum, e.logsch)
+            logarr.push([e.logsch, inum])
+        });
+        const removeembed = new Discord.MessageEmbed()
+        .setTitle(`${emojis.channelredo} Log Settings`)
+        .setDescription(`${logarr.map(a => `\`${a[1]}\` ${message.guild.channels.cache.get(a[0]).name}\n`).join(" ")}`)
         const mentionch = new Discord.MessageEmbed()
             .setTitle(`${emojis.channeladd} Add Log Channel`)
             .setDescription('Please mention a channel.')
             .setColor(color)
         const logembed = new Discord.MessageEmbed()
             .setTitle(`${message.guild}'s Logs`)
+            .addField(`${emojis.good} Active Logs`, `${logs.map(e => `<#${e.logsch}>`).join(" ") || '`There are no active logs`'}`)
             .setColor(color)
         const endembed = new Discord.MessageEmbed()
             .setColor(color)
@@ -39,6 +52,25 @@ module.exports = {
                     .setStyle('SECONDARY')
                     .setLabel('Add Channel'),
                     new MessageButton()
+                    .setCustomId('channels_remove')
+                    .setEmoji(`${emojis.channeldeleteid}`)
+                    .setStyle('SECONDARY')
+                    .setLabel('Remove Channel'),
+                    new MessageButton()
+                    .setCustomId('close')
+                    .setEmoji(`${emojis.leaveid}`)
+                    .setStyle('SECONDARY')
+                    .setLabel('Close'),
+            )
+            const row2dis = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId('channels_add')
+                    .setEmoji(`${emojis.channeladdid}`)
+                    .setStyle('SECONDARY')
+                    .setLabel('Add Channel')
+                    .setDisabled(true),
+                    new MessageButton()
                     .setCustomId('close')
                     .setEmoji(`${emojis.leaveid}`)
                     .setStyle('SECONDARY')
@@ -46,11 +78,31 @@ module.exports = {
             )
         const sm = await message.channel.send({ embeds: [logembed], components: [row2] })
         const filter2 = i => i.user.id === message.author.id;
-
+        const mfilter = i => i.author.id === message.author.id;
+        const filter = i => i.customId === 'logs_select' && i.user.id === message.author.id;
         const collector2 = sm.awaitMessageComponent({ filter2, time: 15000 })
             .then(async i1 => {
                 if (i1.customId === 'close') {
-                    sm.edit({ embeds: [endembed], components: [] })
+                    return sm.edit({ embeds: [removeembed], components: [] })
+
+                } else if (i1.customId === 'channels_remove') {
+                    sm.edit({ embeds: [removeembed], components: [row2dis] })
+                    const collector4 = sm.awaitMessageComponent({ filter2, time: 15000 }).then(async i4 => {
+                        if (i4.customId === 'close') {
+                            return sm.edit({ embeds: [endembed], components: [] })
+                        }}).catch(( )=>{ })
+                    message.channel.awaitMessages({ mfilter, max: 1, time: 900000, errors: ['time'] }).then(async collected3 => {
+                        const tchannel = logco.first(2)[Number(collected3.first().content)-1]
+                        lModel.findOneAndDelete({
+                            guild: message.guild.id,
+                            logsch: tchannel
+                        }, function (err) {
+                            if (err) return handleError(err);
+                            // deleted at most one tank document
+                          });
+                        collected3.first().delete()
+                        return sm.edit({ embeds: [endembed], components: [] })
+                    }).catch(( )=>{ })
                 } else if (i1.customId === 'channels_add') {
 
                     const row = new MessageActionRow()
@@ -107,12 +159,19 @@ module.exports = {
                                 ]),
                         );
                     await sm.edit({ embeds: [mentionch], components: [] });
-                    const mfilter = i => i.author.id === message.author.id;
+                    const collector5 = sm.awaitMessageComponent({ filter2, time: 15000 }).then(async i5 => {
+                        if (i5.customId === 'close') {
+                            return sm.edit({ embeds: [endembed], components: [] })
+                        }})
                     message.channel.awaitMessages({ mfilter, max: 1, time: 900000, errors: ['time'] }).then(async collected2 => {
                         const channel = collected2.first().mentions.channels.first();
                         collected2.first().delete()
                         await sm.edit({ embeds: [logembed], components: [row] });
-                        const filter = i => i.customId === 'logs_select' && i.user.id === message.author.id;
+
+                        const collector6 = sm.awaitMessageComponent({ filter2, time: 15000 }).then(async i6 => {
+                            if (i6.customId === 'close') {
+                                return sm.edit({ embeds: [endembed], components: [] })
+                            }}).catch(( )=>{ })
 
                         const collector = sm.createMessageComponentCollector({ filter, time: 15000 });
 
@@ -271,7 +330,7 @@ module.exports = {
                     }
                     ).catch(( )=>{ sm.edit({ embeds: [endembed], components: [] })})
                 }
-            }).catch(( )=>{ sm.edit({ embeds: [endembed], components: [] })})
+            }).catch((e)=>{ sm.edit({ embeds: [endembed], components: [] }) })
     }
 
 }
