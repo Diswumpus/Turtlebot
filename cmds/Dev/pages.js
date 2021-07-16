7fb0f4809ecb2658c1471a660d45a47fcff881d4
TurtlePaw <turtlesimsyt@gmail.com>
2021-07-16 17:04:39 -0400

    Fix pages.js

--- cmds/Dev/pages.js
+++ cmds/Dev/pages.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const dt = require('../../../discord-turtle/index');
+const dt = require('discord-turtle');
 const emojis = require('../../emojis.json');
 
 module.exports = {
